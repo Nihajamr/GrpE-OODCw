5a9070893b55798c8dc28b6556ae397bee429425
Nihaj Amr <86359042+Nihajamr@users.noreply.github.com>
2021-12-22 00:00:11 +0530

    Update CarParkManager.java

--- src/CarParkManager.java
+++ src/CarParkManager.java
@@ -4,9 +4,9 @@ public interface CarParkManager {
     //void searchVehicle(String regNo);
     void viewLastVehicle();
     void printStatus();
-    void searchByDay(String date);
+    void searchByDay(String date,String fileName);
     void vehiclesByPercentage();
     void charges(String regNo, String etime);
     void longestParked();
-    boolean mainMenu();
+    boolean mainMenu(String fileName);
 }
