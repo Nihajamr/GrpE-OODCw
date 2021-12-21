7abfbc364871ee302a68f940ecea371e44dea302
IBOshadi <86184405+IBOshadi@users.noreply.github.com>
2021-12-21 16:11:06 +0530

    Update CarParkManager.java

--- Oshadi/CarParkManager.java
+++ Oshadi/CarParkManager.java
@@ -1,1 +1,22 @@
+public interface CarParkManager {
+    void addVehicle(Vehicle vehicle);
+    void deleteVehicle(String RegNO);
+    void findVehicle(String RegNo);
+    void printPercentage();
+    void printReceipt();
+    boolean details();
+}
+class Main {
+    public static void main(String[] args){
+        System.out.println("=================================================================");
+        System.out.println("                    WELCOME TO THE CAR PARK");
+        System.out.println("=================================================================");
+        System.out.println("");
+        CarParkManager carPark = new IITCarParkManager();
+        boolean exit = false;
+        while (!exit){
+            exit = carPark.details();
+        }
+    }
+}
 
